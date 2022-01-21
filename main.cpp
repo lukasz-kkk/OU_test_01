39af6e3e0f0bac403261396841faecffc6572fd0
lukasz-kkk <lukaluzny17@gmail.com>
2022-01-21 23:11:10 +0100

    Zmiana dodawania na mnozenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int policz (int a, int b) {
-    return a+b;
+    return a*b; //zamiana dodawania na mnożenie
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std:: cout <<"W pierwszym branchu; 2+3=" << policz(2,3) <<std::endl;
+    std:: cout <<"W pierwszym branchu; 2*3=" << policz(2,3) <<std::endl;
     return 0;
 }
